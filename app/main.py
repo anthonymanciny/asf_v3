ff9894415bc68cbb9a31f29ce2fa3ab265cdb9bb
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-12-06 14:41:39 -0400

    cors ok

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from app.routes.aapp_router import api_router
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
+# Configuração do CORS para permitir o frontend em localhost
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # Permitir somente o frontend Angular
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos HTTP
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
 
 
 
